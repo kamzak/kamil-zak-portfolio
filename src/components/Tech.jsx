7e0bf7373b6cf543ea303959024ad5d8366caa47
kamzak <58105737+kamzak@users.noreply.github.com>
2023-06-21 18:56:04 +0200

    Fix technology balls on mobile

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -4,8 +4,32 @@ import { technologies } from "../constants";
 import { motion } from "framer-motion";
 import { textVariant } from "../utils/motion";
 import { styles } from "../styles";
+import { useEffect, useState } from "react";
+
+const TechMobileSphere = ({ icon }) => (
+  <div className="w-24 h-24 drop-shadow-[0_15px_25px_rgba(255,255,255,0.25)] ">
+    <img src={icon} alt="icon" className="w-full h-full object-contain" />
+  </div>
+);
 
 const Tech = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (e) => {
+      setIsMobile(e.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <div>
       <motion.div variants={textVariant()}>
@@ -17,7 +41,11 @@ const Tech = () => {
       <div className="flex flex-row flex-wrap justify-center gap-10 mt-20">
         {technologies.map((technology) => (
           <div key={technology.name} className="w-28 h-28">
-            <BallCanvas icon={technology.icon} />
+            {isMobile ? (
+              <TechMobileSphere icon={technology.icon} />
+            ) : (
+              <BallCanvas icon={technology.icon} />
+            )}
             <p className="text-center text-white-100 text-[14px] mt-2">
               {technology.name}
             </p>
